ff3686d055e2a018bc6a6129686fd02d97c8281d
CarlosRFS <carlosrfs99@gmail.com>
2020-02-06 21:20:04 -0300

    header tiro.h

---
+++ tiro.h
@@ -0,0 +1,21 @@
+#include "campo.h"
+
+struct Tiro {
+	int pos;
+
+	Tiro(int &body) {
+		pos=body - c1.width + 1;
+	}
+
+	void draw_on_campo(Campo &c1) {
+		c1.campo[pos] = x;
+	}
+
+	void disparo(Campo &c1) {
+		for(;;) {
+			draw_on_campo(c1);
+			pos++;
+			if(c1.campo[pos] == 26) break;
+		}
+	}
+};
