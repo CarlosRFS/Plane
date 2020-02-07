072738a7c445565a3c76af53e871c5977997e564
CarlosRFS <carlosrfs99@gmail.com>
2020-02-07 09:33:34 -0300

    desenho e movimentação do aviao completas

--- aviao.h
+++ aviao.h
@@ -7,16 +7,24 @@ struct Aviao {
 	int body;
 	char caracter = 26;
 	Aviao(Campo &c1, char &caracter) {
-		body = 1300; 
+		body = 1300; //posiciona corretamente apenas em um campo 50x30 
 		draw_on_campo(c1);
 	}
 
 	void draw_on_campo(Campo &c1) {
 		for(int i = 0; i < 4; i++) c1.campo[body + (i * (c1.width + 1))] = caracter;
+
+		for(int i = 0; i < 7; i++) c1.campo[body + c1.width - 2 + i] = caracter;
+
+		for(int i = 0; i < 3; i++) c1.campo[body + 3 * c1.width + 2 + i] = caracter;
 	}
 
 	void erase_on_campo(Campo &c1) {
 		for(int i = 0; i < 4; i++) c1.campo[body + (i * (c1.width + 1))] = ' ';
+
+		for(int i = 0; i < 7; i++) c1.campo[body + c1.width - 2 + i] = ' ';
+
+		for(int i = 0; i < 3; i++) c1.campo[body + 3 * c1.width + 2 + i] = ' ';
 	}
 	
 	void move_e(Campo &c1) {
